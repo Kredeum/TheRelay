83c287ae43efabab12809b01942fac0564952ea4
zapaz.eth <alain@kredeum.com>
2022-07-23 12:30:05 +0200

    list erc721 and erc1155 tokens uri

--- express/index.ts
+++ express/index.ts
@@ -2,21 +2,68 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { schema } from "./schema";
 import { remoteGraphQL } from "./remote";
-import proxy from "express-http-proxy";
+
+import dotenv from "dotenv";
+import findupSync from "findup-sync";
+
+if (!process.env.PUBKEY) {
+  dotenv.config({ path: findupSync(".env") || "" });
+  if (!process.env.PUBKEY) {
+    throw new Error("HARDHAT : ENV variable PUBKEY not set!");
+  }
+}
+const PUBKEY = process.env.PUBKEY.toLowerCase();
 
 const PORT = 4000;
 const app = express();
-const URL = "https://api.publicapis.org";
+// const URL = "https://api.publicapis.org";
+
+const ENDPOINT = "https://api.thegraph.com/subgraphs/name/amxx/nft-matic";
+const QUERY = `{
+  account(id: "${PUBKEY}") {
+    ERC721tokens(first: 9) {
+      uri
+    }
+    ERC1155balances(first: 9) {
+      token{
+        uri
+      }
+    }
+  }
+}`;
+console.log("QUERY", QUERY);
+
+type InputType = {
+  data: {
+    account: {
+      ERC721tokens: UriType;
+      ERC1155balances: UriBalType;
+    };
+  };
+};
+type UriType = Array<{
+  uri: string;
+}>;
+type UriBalType = Array<{
+  token: { uri: string };
+}>;
+type OutputType = Array<string>;
+
+const transform = (input: UriType): OutputType => input.map((item) => item.uri);
+
+app.use("/api", async (req, res) => {
+  const input: string = await remoteGraphQL(ENDPOINT, QUERY);
+  const { account } = (JSON.parse(input) as InputType).data;
+  const out721 = account.ERC721tokens.map((tok) => tok.uri);
+  const out1155 = account.ERC1155balances.map((tok) => tok.token.uri);
+
+  let out = [...new Set([...out721, ...out1155])];
+  res.send(JSON.stringify(out, null, " "));
+});
 
 app.use("/hi", (req, res) => {
   res.send("Bonjour!");
 });
-
-app.use("/api", async (req, res) => {
-  res.send(await remoteGraphQL());
-});
-
-// app.use("/", proxy(URL));
 
 app.use(
   "/graphql",
--- express/remote.ts
+++ express/remote.ts
@@ -1,14 +1,6 @@
 import fetch from "node-fetch";
 
-const URL = "https://api.thegraph.com/subgraphs/name/tranchien2002/eip721-matic";
-const QUERY = `{
-  alls
-  {
-    numTokens
-  }
-}`;
-
-async function remoteGraphQL(query: string = QUERY, url: string = URL): Promise<string> {
+async function remoteGraphQL(url: string, query: string): Promise<string> {
   console.log(`${query}\n${url}`);
 
   const config = { method: "POST", body: JSON.stringify({ query: query }) };
